a09e0a9f0676345eeff3b9246d78ac778ed35c49
MeridjanShoti <meridjanshoti@gmail.com>
2025-02-21 16:55:17 +0100

    a

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -2,6 +2,7 @@ export const SET_SEARCH = "SET_SEARCH";
 export const SET_SELECTED_SONG = "SET_SELECTED_SONG";
 export const ADD_TO_FAVOURITES = "ADD_TO_FAVOURITES";
 export const REMOVE_FROM_FAVOURITES = "REMOVE_FROM_FAVOURITES";
+export const SET_SEARCHED_RESULTS = "SET_SEARCHED_RESULTS";
 
 export const setSearchAction = (search) => {
   return (dispatch) => {
@@ -21,5 +22,10 @@ export const removeFromFavourites = (el) => {
 export const selectSong = (song) => {
   return (dispatch) => {
     dispatch({ type: SET_SELECTED_SONG, payload: song });
+  };
+};
+export const setSearchedResultsAction = (results) => {
+  return (dispatch) => {
+    dispatch({ type: SET_SEARCHED_RESULTS, payload: results });
   };
 };
