5b1326ef8305c7738195d66c2947850137399f31
MeridjanShoti <meridjanshoti@gmail.com>
2025-02-21 16:15:14 +0100

    sistemazione player

--- src/components/MyPlayer.jsx
+++ src/components/MyPlayer.jsx
@@ -3,14 +3,28 @@ import prev from "../assets/prev.png";
 import play from "../assets/play.png";
 import next from "../assets/next.png";
 import repeat from "../assets/repeat.png";
-
+import { Col } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import musicPlaceholder from "../assets/musicPlaceholder.jpg";
 const MyPlayer = () => {
+  const playingSong = useSelector((state) => state.selectedSong.content);
   return (
     <>
       <div className="container-fluid fixed-bottom bg-container pt-1">
         <div className="row h-100">
           <div className="col-lg-10 offset-lg-2">
-            <div className="row h-100 flex-column justify-content-center align-items-center">
+            <div className="row h-100 justify-content-start align-items-center">
+              <div className="col-4 d-flex align-items-center justify-content-center gap-5">
+                <img
+                  src={playingSong ? playingSong.album.cover : musicPlaceholder}
+                  alt={playingSong && playingSong.title}
+                  height={"50px"}
+                />
+                <div>
+                  {playingSong && <p className="text-white">{playingSong.title}</p>}
+                  {playingSong && <p className="text-white">{playingSong.artist.name}</p>}
+                </div>
+              </div>
               <div className="col-6 col-md-4 playerControls">
                 <div className="d-flex">
                   <a href="#">
