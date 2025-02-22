07ba87cd9d42109ad114241a601a5e3902c04f9b
MeridjanShoti <meridjanshoti@gmail.com>
2025-02-22 14:44:42 +0100

    a

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,36 @@ import "./App.css";
 import MyHomePage from "./components/MyHomePage";
 import MyFavourites from "./components/MyFavourites";
 import MyUovoDiPasqua from "./components/MyUovoDiPasqua";
+import { Container } from "react-bootstrap";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<MyHomePage />} />
-          <Route path="/favourites" element={<MyFavourites />} />
-          <Route path="*" element={<MyUovoDiPasqua />} />
+          <Route
+            path="/"
+            element={
+              <Container fluid>
+                <MyHomePage />
+              </Container>
+            }
+          />
+          <Route
+            path="/favourites"
+            element={
+              <Container fluid>
+                <MyFavourites />
+              </Container>
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <Container fluid>
+                <MyUovoDiPasqua />
+              </Container>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </>
